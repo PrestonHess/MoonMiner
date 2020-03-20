e7c656d3cc4b99732b58910de86d711b51cd74d5
Preston Hess <hesspreston@outlook.com>
2020-03-20 16:03:49 -0700

    update

---
+++ app.js
@@ -0,0 +1,73 @@
+let clickUpgrades = {
+  pickaxes: {
+    price: 100,
+    quantity: 0,
+    multiplier: 1
+  },
+  laser: {
+    price: 1000,
+    quantity: 0,
+    multiplier: 5
+  }
+};
+
+let automaticUpgrades = {
+  rovers: {
+    price: 600,
+    quantity: 0,
+    multiplier: 20
+  }
+};
+
+let cheese = 1000;
+
+// NOTE This function is invoked when the user clicks on the moon which increases cheese count and updates the DOM
+function mine() {
+  let pickaxeMultiplier = clickUpgrades.pickaxes.quantity * clickUpgrades.pickaxes.multiplier;
+  let laserMultiplier = clickUpgrades.laser.quantity * clickUpgrades.laser.multiplier;
+  cheese += 1 + pickaxeMultiplier + laserMultiplier;
+  update();
+}
+
+//NOTE This function is responsible for updating the DOM with the current cheese count 
+function update() {
+  document.getElementById('cheese-Count').innerText = cheese.toString();
+  document.getElementById('pickaxe-Count').innerText = clickUpgrades.pickaxes.quantity.toString();
+  document.getElementById('pickaxe-Price').innerText = clickUpgrades.pickaxes.price.toString();
+  document.getElementById('laser-Count').innerText = clickUpgrades.laser.quantity.toString();
+  document.getElementById('laser-Price').innerText = clickUpgrades.laser.price.toString();
+}
+
+
+// NOTE This method will be responsible for checking if the user has the resources, and if they do increasing the pickaxe purchased count, and decreasing the cheese resources by the appropriate amount.
+function BuyPickaxe() {
+  let price = clickUpgrades.pickaxes.price;
+  if (cheese >= price) {
+    cheese -= price;
+    clickUpgrades.pickaxes.quantity++;
+    clickUpgrades.pickaxes.price += Math.floor(price * 0.2);
+    update();
+  } else {
+    alert('You need more cheese');
+  }
+}
+
+function BuyLaser() {
+  console.log("purhcased")
+  let price = clickUpgrades.laser.price;
+  if (cheese >= price) {
+    cheese -= price;
+    clickUpgrades.laser.quantity++;
+    clickUpgrades.laser.price += Math.floor(price * 0.2);
+    update();
+  } else {
+    alert('You need more cheese');
+  }
+}
+
+function collectAutoUpgrades() {
+  
+}
+
+
+
